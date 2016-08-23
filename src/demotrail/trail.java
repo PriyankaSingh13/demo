948abb8dd49c3810834e9f6aeed412fcad62247e
priyanka.singh <priyanka.singh@Tsui>
2016-08-23 17:07:22 +0530

    second commit

--- src/demotrail/trail.java
+++ src/demotrail/trail.java
@@ -4,7 +4,7 @@ public class trail {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+/// this is my second commit
 		System.out.println("Hello World");
 		System.out.println("Welcome");
 	}
