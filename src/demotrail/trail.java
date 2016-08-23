a07de1fd21acf8dddd979f6616926304326d4034
priyanka.singh <priyanka.singh@Tsui>
2016-08-23 16:51:54 +0530

    first commit

---
+++ src/demotrail/trail.java
@@ -0,0 +1,12 @@
+package demotrail;
+
+public class trail {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Hello World");
+		System.out.println("Welcome");
+	}
+
+}
